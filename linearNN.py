8d0f9287f9128d79015dd061f9d3aaf0956c0410
SheldonVon <88470100+SheldonVon98@users.noreply.github.com>
2022-01-24 14:42:30 +0800

    update

--- linearNN.py
+++ linearNN.py
@@ -5,7 +5,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-
+from matplotlib import animation
 """
              x_1      neuron_11
     X   =>   x_2  =>  neuron_12 =>  neuron_21 => Y
@@ -88,6 +88,7 @@ class ReLu():
     def differentiate(self):
         """docstring for differentiate"""
         self.dafo = self.afo.copy()
+        self.dafo[self.dafo<=0] = 0
         self.dafo[self.dafo>0] = 1
         return self.dafo
 
@@ -95,7 +96,9 @@ class ReLu():
         """docstring for show"""
         x = np.linspace(-10, 10, 100)
         y = self(x)
+        dy = self.differentiate()
         plt.plot(x, y)
+        plt.plot(x, dy)
         plt.show()
 
 #ReLu().show()
@@ -122,13 +125,17 @@ class Sigmoid():
         """docstring for show"""
         x = np.linspace(-10, 10, 100)
         y = self(x)
+        dy = self.differentiate()
         plt.plot(x, y)
+        plt.plot(x, dy)
         plt.show()
 
+#Sigmoid().show()
+#exit(0)
 
 class MeanSquareLoss():
     """
-        Mean Square Loss
+ linearlarity       Mean Square Loss
     """
 
     # Derivative of Loss with respect 
@@ -185,9 +192,9 @@ class Neuron():
     inputs = 0
 
     def __init__(self, dim: int, bias: bool):
-        self.weight = np.random.randn((dim))
+        self.weight = np.random.randn(dim, 1)
         if bias:
-            self.bias = np.random.random(1)[0]
+            self.bias = np.random.random(1)
 
     def __call__(self, x):
         """
@@ -196,30 +203,41 @@ class Neuron():
             x       => (dim)
             weight  => (dim)
         """
-        z = np.dot(x, self.weight) + self.bias
-        return z
-
-    def differenciate_wrt_weight(self, x, dO, db):
-        """docstring for differentiate"""
-        print("x",x.shape)
-        print("dO",dO.shape)
-        self.dw = dO.T.dot(x)[0]
-        self.db = db.sum()
-        print(db.shape)
         #print("b", self.bias)
-        #print("dw", self.dw)
-        print("db", db)
+        #print("x", x.shape)
+        #print("w", self.weight.shape)
+        #print("b", self.bias.shape)
+        z = self.weight.T.dot(x) + self.bias
+        #print(z)
+        return z[0]
+
+    def differenciate(self, x, dO, idx):
+        """docstring for differentiate"""
+        #print("x",x.shape)
+        #print("dO",dO.shape)
+        #print("dO", dO)
+        #print(idx)
+        #print("x.T.dot(dO)", x.T.dot(dO).shape)
+        self.dw = x.T.dot(dO)[:, idx].reshape(-1, 1)
+        self.db = np.sum(dO, axis=0)[idx]
+        #print(dO)
+        #print(dO.shape)
+        #print("b", self.bias.shape)
+        #print("dw", self.dw.shape)
+        #print("db", self.db)
         #exit(0)
         #print("dw", self.dw)
-        #print('w', self.weight)
+        #print('w', self.weight.shape)
         # return self.weight
+        #print("\n")
 
     def update(self, lr):
         """docstring for update"""
-        #print("dw", self.dw)
+        #print("w", self.weight.shape)
+        #print("dw", self.dw.shape)
         self.weight -= lr*self.dw
         self.bias -= lr*self.db
-
+        #print(self.bias)
 
 class LinearLayer():
     """
@@ -246,22 +264,27 @@ class LinearLayer():
         self.inputs = x
         self.outputs = np.array([[neuron(p) for neuron in self.neurons] for p
                                  in self.inputs])
+        #print("\nop", self.outputs.shape)
         if self.activation is not None:
             self.outputs = self.activation(self.outputs)
         return self.outputs
 
     def differentiate(self, dO, lr):
-        #print("dO", dO)
+        #print("\n\ndO__", dO)
         if self.activation is not None:
             #print("da", self.activation.differentiate())
             #print("dOda", dO *self.activation.differentiate())
-            dO = dO * self.activation.differentiate()
-            # print("dO",dO)
+            #print("dO", dO.shape)
+            #print("ad", self.activation.differentiate().shape)
+            dO *= self.activation.differentiate()
+            #print("dO",dO.shape)
         for idx, neural in enumerate(self.neurons):
-            neural.differenciate_wrt_weight(self.inputs, dO, dO[:, idx])
+            neural.differenciate(self.inputs, dO, idx)
             # print("dO",dO)
         #print(np.array([neural.weight for neural in self.neurons]))
-        di = dO.dot(np.array([neural.weight for neural in self.neurons]))
+        #print("ns", np.array([neural.weight[:, 0] for neural in self.neurons]).shape)
+
+        di = dO.dot(np.array([neural.weight[:, 0] for neural in self.neurons]))
         # print(di)
         self.update(lr)
         return di
@@ -275,27 +298,17 @@ class LinearLayer():
 class NeuralNetwork():
     """docstring for NeuralNetwork"""
 
-    def __init__(self, lr=0.05, data=DataGenRegSin(num=100)):
+    def __init__(self, lr=0.0005, data=DataGenRegSin(num=100)):
         self.data = data
         self.lr = lr
         self.tr_x, self.tr_y, self.te_x, self.te_y = self.data.split()
         self.lossFunc = MeanSquareLoss()
 
         self.sequential = [
-            LinearLayer(in_dim=1, out_dim=30, bias=True,
-                        activation=ReLu()),
-            LinearLayer(in_dim=30, out_dim=1, bias=True,
-                        activation=None),
+            LinearLayer(in_dim=1, out_dim=50, bias=True, activation=ReLu()),
+            LinearLayer(in_dim=50, out_dim=1, bias=True, activation=None),
         ]
-
-        # self.sequential = [
-        #    LinearLayer(in_dim=1, out_dim=1, bias=False, activation=None),
-        #    ]
-        # self.sequential = [
-        #    LinearLayer(in_dim=self.tr_x.shape[1], out_dim=2, bias=True,
-        #        activation=None),
-        #    LinearLayer(in_dim=2, out_dim=1, bias=True, activation=None),
-        # ]
+        self.vis_init()
 
     def forward(self, x):
         """docstring for forward"""
@@ -308,33 +321,60 @@ class NeuralNetwork():
         d_loss_d_y_pre = self.lossFunc.differentiate()
         dO = d_loss_d_y_pre
         for layer in self.sequential[::-1]:  # Traverse the layers reversely
-            # print(layer)
+            #print(layer)
             dO = layer.differentiate(dO.copy(), self.lr)
         # exit()
 
-    def train(self, epochs=1000):
+    def train(self, epochs=20000):
         """docstring for train"""
+        #self.fig.canvas.draw()
         for epoch in range(epochs):
             output = self.forward(self.tr_x)
             loss = self.lossFunc(output, self.tr_y)
             self.backward(loss)
-            if epoch % 50 == 0:
-                self.lr *= 0.7
+            self.vis()
+            if epoch > 2000:
+                if epoch % 100 == 0:
+                    self.lr *= 0.98
             print("epoch: {} loss: {} lr: {}".format(epoch, loss, self.lr))
 
     def test(self):
         """docstring for test"""
         return
 
+    def vis_init(self):
+        self.fig, self.ax = plt.subplots()
+        self.ax.set_xlim(0, 1)
+        self.ax.set_ylim(0, 1)
+        x = np.array([np.linspace(0, 1, 30)]).reshape(-1, 1)
+        y = self.forward(x)
+        self.ax.scatter(self.tr_x, self.tr_y, animated=False)
+        self.ln = self.ax.plot(x, y, animated=True)[0]
+        plt.show(block=False)
+        plt.pause(0.1)
+        # get copy of entire figure (everything inside fig.bbox) sans animated artist
+        self.bg = self.fig.canvas.copy_from_bbox(self.fig.bbox)
+        # draw the animated artist, this uses a cached renderer
+        self.ax.draw_artist(self.ln)
+        # show the result to the screen, this pushes the updated RGBA buffer from the
+        # renderer to the GUI framework so you can see it
+        self.fig.canvas.blit(self.fig.bbox)
+
     def vis(self):
         """docstring for vis"""
         x = np.array([np.linspace(0, 1, 30)]).reshape(-1, 1)
         y = self.forward(x)
-        plt.plot(x, y)
-        plt.scatter(self.tr_x, self.tr_y)
-        plt.show()
-
+        self.fig.canvas.restore_region(self.bg)
+        # update the artist, neither the canvas state nor the screen have changed
+        self.ln.set_ydata(y)
+        # re-render the artist, updating the canvas state, but not the screen
+        self.ax.draw_artist(self.ln)
+        # copy the image to the GUI state, but screen might not be changed yet
+        self.fig.canvas.blit(self.fig.bbox)
+        # flush any pending GUI events, re-painting the screen if needed
+        self.fig.canvas.flush_events()
 
 nn = NeuralNetwork()
 nn.train()
-nn.vis()
+#nn.vis()
+plt.show()
